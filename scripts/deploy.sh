49eb7959f5524500c570e9b46551e27f258c74c8
Uilian Ries <uilian@khomp.com>
2017-11-14 16:05:40 -0200

    Push after success on master
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-docker login -p ${DOCKER_PASSWORD} -u ${DOCKER_USERNAME}
-docker-compose push
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    docker login -p ${DOCKER_PASSWORD} -u ${DOCKER_USERNAME}
+    docker-compose push
+fi
+
+exit 0
