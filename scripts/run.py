48dceab1f32aba3a2e380de0641209431445646a
Uilian Ries <uilianries@gmail.com>
2017-11-16 17:40:58 -0200

    Test image before to build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- scripts/run.py
+++ scripts/run.py
@@ -3,34 +3,72 @@
 import os
 import sys
 import subprocess
+import platform
+import re
+
+class Runner(object):
+
+    def __init__(self):
+        self.username = os.getenv("DOCKER_USERNAME", "")
+        self.password = os.getenv("DOCKER_PASSWORD", "").replace('"', '\\"')
+        self.upload = os.getenv("DOCKER_UPLOAD", False)
+        travis_branch = os.getenv("TRAVIS_BRANCH", "")
+        appveyor_branch = os.getenv("APPVEYOR_REPO_BRANCH","")
+        self.stable_branch = os.getenv("DOCKER_STABLE_BRANCH", "master")
+        self.service = os.getenv("DOCKER_SERVICE", "")
+        self.branch = travis_branch if travis_branch != "" else appveyor_branch
+        self.compiler = "Visual Studio" if platform.system() == "Windows" else "gcc"
+        version_match = re.match(r"\D+(\d+).*", self.service)
+        self.compiler_version = version_match.group(1) if platform.system() == "Windows" else '.'.join(version_match.group(1))
+
+    def build(self):
+        subprocess.check_call(["docker-compose", "build", self.service])
+
+    def test(self):
+        subprocess.check_call(["docker-compose", "up", "-d", self.service])
+        subprocess.check_call(["docker-compose", "exec", self.service, "sudo", "pip", "install", "-U", "conan_package_tools"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "sudo", "pip", "install", "-U", "conan"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "conan", "user"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "conan", "install", "zlib/1.2.11@conan/stable",
+                                "-s", "arch=x86_64", "-s", "compiler=%s" % self.compiler, "-s", "compiler.version=%s" % self.compiler_version,
+                                "--build"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "conan", "install", "zlib/1.2.11@conan/stable",
+                                "-s", "arch=x86", "-s", "compiler=%s" % self.compiler, "-s", "compiler.version=%s" % self.compiler_version,
+                                "--build"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "conan", "remote", "add", "conan-community",
+                               "https://api.bintray.com/conan/conan-community/conan", "--insert"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "conan", "install", "gtest/1.8.0@conan/stable",
+                                "-s", "arch=x86_64", "-s", "compiler=%s" % self.compiler, "-s", "compiler.version=%s" % self.compiler_version,
+                                "--build"])
+        subprocess.check_call(["docker-compose", "exec", self.service, "conan", "install", "gtest/1.8.0@conan/stable",
+                                "-s", "arch=x86", "-s", "compiler=%s" % self.compiler, "-s", "compiler.version=%s" % self.compiler_version,
+                                "--build"])
+
+    def deploy(self):
+        if not self.upload:
+            print("Skiping upload, the option is disabled.")
+            sys.exit(0)
+
+        if self.stable_branch != self.branch:
+            print("Skiping upload, branch %s is not stable" % self.branch)
+            sys.exit(0)
+
+        if not self.username:
+            print("Could not upload, username was not defined.")
+            sys.exit(1)
+
+        if not self.password:
+            print("Could not upload, password was not defined.")
+            sys.exit(1)
+
+        subprocess.check_call(["docker", "login", "-u", self.username, "-p", self.password])
+        subprocess.check_call(["docker-compose", "push", self.service])
+
+    def run(self):
+        self.build()
+        self.test()
+        self.deploy()
 
 if __name__ == "__main__":
-    username = os.getenv("DOCKER_USERNAME", "")
-    password = os.getenv("DOCKER_PASSWORD", "").replace('"', '\\"')
-    upload = os.getenv("DOCKER_UPLOAD", False)
-    travis_branch = os.getenv("TRAVIS_BRANCH", "")
-    appveyor_branch = os.getenv("APPVEYOR_REPO_BRANCH","")
-    stable_branch = os.getenv("DOCKER_STABLE_BRANCH", "master")
-    service = os.getenv("DOCKER_SERVICE", "")
-    branch = travis_branch if travis_branch != "" else appveyor_branch
-
-    subprocess.check_call(["docker-compose", "build", service])
-
-    if not upload:
-        print("Skiping upload, the option is disabled.")
-        sys.exit(0)
-
-    if stable_branch != branch:
-        print("Skiping upload, branch %s is not stable" % branch)
-        sys.exit(0)
-
-    if not username:
-        print("Could not upload, username was not defined.")
-        sys.exit(1)
-
-    if not password:
-        print("Could not upload, password was not defined.")
-        sys.exit(1)
-
-    subprocess.check_call(["docker", "login", "-u", username, "-p", password])
-    subprocess.check_call(["docker-compose", "push", service])
+    runner = Runner()
+    runner.run()
